abe1788919851d2bf9ed3aff96a30a45a9df3fce
Tim Eades <tim.eades@mac.com>
2024-01-12 18:29:03 +0000

    initial setup, structure and some psudo code

---
+++ assets/js/logic.js
@@ -0,0 +1,16 @@
+/* AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers */
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN all questions are answered
+// THEN the timer stops and I am presented with a final score
+// WHEN the timer expires
+// THEN the game is over
+
+
---
+++ assets/js/questions.js
@@ -0,0 +1,2 @@
+// 1. Create some objects to hold the questions and answers.
+// 2. Create a function to display the questions and answers.
